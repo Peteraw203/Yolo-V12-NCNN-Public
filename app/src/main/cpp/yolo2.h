42192ea1b9172defea48c59264f98d55cc07a694
Peteraw203 <161308385+Peteraw203@users.noreply.github.com>
2025-10-04 00:51:53 +0700

    Training data
    yolov12n = best.pt (ncnn)
    backup = pretrain coco
    yolo.h -> 5 kelas

--- app/src/main/cpp/yolo2.h
+++ app/src/main/cpp/yolo2.h
@@ -7,13 +7,20 @@
 #include <net.h>
 
 static const char *class_names[] = {
-        "aspal",
-        "keramik",
-        "lubang",
-        "paving",
-        "uneven"
+		"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+		"traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+		"dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+		"umbrella", "handbag",
+		"tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+		"baseball glove", "skateboard",
+		"surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon",
+		"bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza",
+		"donut", "cake",
+		"chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
+		"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+		"book", "clock", "vase", "scissors",
+		"teddy bear", "hair drier", "toothbrush"
 };
-
 
 static const unsigned char colors[19][3] = {
 		{54,  67,  244},
